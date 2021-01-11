fc9e36855a3a4af4e69ae0861a9c436fc1a15e8e
Eppa Lea Turniawan <eppalea@gmail.com>
2021-01-11 17:35:19 +0000

    compared console.log vs return in sayHello function

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
+/*
 const sayHello = function (name) {
   console.log("Hello, " + name );
 }
@@ -5,3 +6,15 @@ const sayHello = function (name) {
 sayHello("Eppa Lea");
 sayHello("Melissa");
 sayHello("Chelsea");
+*/
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
