5698847f3372416beda45c8581ce07d668e67395
Eppa Lea Turniawan <eppalea@gmail.com>
2021-01-12 01:06:11 +0000

    tinkered with examples of forEach and .map() loops

---
+++ forEach-loops.js
@@ -0,0 +1,20 @@
+//forEach
+
+var donuts = ["jelly donut", "chocolate donut", "glazed donut"];
+
+donuts.forEach(function(donut) {
+  donut += " hole";
+  donut = donut.toUpperCase();
+  console.log(donut);
+});
+
+//.map()
+var donuts = ["jelly donut", "chocolate donut", "glazed donut"];
+
+var improvedDonuts = donuts.map(function(donut) {
+  donut += " hole";
+  donut = donut.toUpperCase();
+  return(donut);
+});
+
+console.log(improvedDonuts);
