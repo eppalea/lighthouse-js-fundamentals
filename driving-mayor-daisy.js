d15d57d58054d7590a2bfb9fa042b23b8c671b88
Eppa Lea Turniawan <eppalea@gmail.com>
2021-01-13 07:16:00 +0000

    write a function, carPassing(cars, speed), that will create a new object with two properties. this function should return an array that includes all the elements in car as well as our new element

---
+++ driving-mayor-daisy.js
@@ -0,0 +1,26 @@
+const carPassing = function (cars, speed) {
+  cars.push({    //create a new object with properties time and speed
+    time: Date.now(),
+    speed: speed
+  });
+  return cars; //returns the new cars object when you console.log(cars) outside the function
+  }
+  const cars = [
+    {
+      time: 1568329654807,
+      speed: 40,
+    },
+    {
+      time: 1568329821632,
+      speed: 42,
+    },
+    {
+      time: 1568331115463,
+      speed: 35
+    }
+  ]
+  
+  const speed = 38
+  
+carPassing(cars,speed);
+console.log(cars);
