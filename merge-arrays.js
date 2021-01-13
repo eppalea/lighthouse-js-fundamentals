b108041b81c2ef7dd85dcff25eeeb52b327c1d83
Eppa Lea Turniawan <eppalea@gmail.com>
2021-01-13 00:01:11 +0000

    defined a function, which when given two arrays containing numbers, returned a sorted array of the numbers from both lists

---
+++ merge-arrays.js
@@ -0,0 +1,12 @@
+function merge(array1, array2) {
+  let bothArrays = array1.concat(array2);
+  return bothArrays.sort();  
+}
+
+
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+/*
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
+*/
